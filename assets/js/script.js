4823303332d9feae8a690a35403a2f98be4230a9
onur-CK <cpkonr@gmail.com>
2024-03-29 07:04:44 +0100

    Rules window display
    Add js and html changes to display quickz rules

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Wait for the DOM content to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Quickz rules button
+  const rulesBtn = document.getElementById('rules-btn');
+
+  // Quickz rules div
+  const rulesDiv = document.getElementById('quickz-rules')
+
+  // Add click event listener to the rules button
+  rulesBtn.addEventListener('click', function() {
+    // Toggle  the display of rules div
+    if(rulesDiv.style.display === 'none') {
+      rulesDiv.style.display = 'block'; 
+    } else {
+      rulessDiv.style.display = 'none';
+    }
+    
+  });
+});
