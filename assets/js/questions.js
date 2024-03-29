a0d8c2f05c25692ee1b149dad030676def8c4b54
onur-CK <cpkonr@gmail.com>
2024-03-29 09:32:35 +0100

    Js questions
    Add changes to create js questions file and create the const questions.

---
+++ assets/js/questions.js
@@ -0,0 +1,11 @@
+const questions = [
+  {
+    question: "Who wrote the novel "1984",
+    answers: [
+      { text: "George Orwell", correct: true},
+      { text: "J.K. Rowling", correct: false}
+      { text: "F. Scott Fitzgerald", correct: false}
+      { text: "Ernest Hemingway", correct: false}
+    ]
+  }
+]
