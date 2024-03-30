f7aa6d2b689a646c4455cf4e436bea1600eaf0fd
onur-CK <cpkonr@gmail.com>
2024-03-30 12:04:02 +0100

    Html game-area and question area
    Add changes for game-area and question-area layouts for html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
 
-
-
 // Wait for the DOM content to be fully loaded
 document.addEventListener('DOMContentLoaded', function() {
   // Quickz rules button
   const rulesBtn = document.getElementById('rules-btn');
 
   // Quickz rules div
-  const rulesDiv = document.getElementById('quickz-rules')
+  const rulesDiv = document.getElementById('quickz-rules');
 
   // Add click event listener to the rules button
   rulesBtn.addEventListener('click', function() {
-    // Toggle  the display of rules div
-    if(rulesDiv.style.display === 'none') {
-      rulesDiv.style.display = 'block'; 
+    // Toggle the display of rules div
+    if (rulesDiv.style.display === 'none' || rulesDiv.style.display === '') {
+      rulesDiv.style.display = 'block';
     } else {
       rulesDiv.style.display = 'none';
     }
-    
   });
 });
+
+
+
+
+
+
+
+
