4770fb679e358add9d75ba91f81ac2c74d4900ee
onur-CK <cpkonr@gmail.com>
2024-12-18 04:28:10 +0100

    serStatusClass
    Add changes to make setStatusClass function handle removing classes in a single line by using toggle()

--- assets/js/script.js
+++ assets/js/script.js
@@ -313,12 +313,8 @@ function endScore() {
 }
 
 function setStatusClass(element, correct) {
-  clearStatusClass(element);
-  if (correct) {
-    element.classList.add("correct");
-  } else {
-    element.classList.add("wrong");
-  }
+  element.classList.toggle("correct", !!correct);
+  element.classList.toggle("wrong", !correct);
 }
 
 function clearStatusClass(element) {
